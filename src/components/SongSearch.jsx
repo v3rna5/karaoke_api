5b39c8a084476f3c688930e023f3c61d2bb94cfc
verna <vernajs@gmail.com>
2018-07-17 09:24:08 -0700

    Monday July16Lesson

---
+++ src/components/SongSearch.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { fetchSongId } from './../actions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function SongSearch({ dispatch }){
+  let input;
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault();
+        if (!input.value.trim()) {
+          return;
+        }
+        dispatch(fetchSongId(input.value.trim()));
+        // ...we'll add more custom logic here later...
+        console.log('SEARCHED ARTIST:');
+        console.log(input.value.trim());
+        //...instead of these console.log()s....
+        input.value = '';
+      }}>
+        <input placeholder="Song Title" ref={node => {
+          input = node;
+        }}></input>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
+
+SongSearch.propTypes = {
+  dispatch: PropTypes.func
+};
+
+export default connect()(SongSearch);
